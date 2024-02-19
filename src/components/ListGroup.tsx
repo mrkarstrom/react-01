74387719b5c641176dbe74ee901475db467b907a
Magnus Karström <mrkarstrom81@gmail.com>
2024-02-19 12:44:34 +0100

    add (ListGroup.tsx): Using a functional approach for messaging on conditional rendering.

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -2,10 +2,23 @@ function ListGroup() {
   let items = ['New York', 'Las Vegas', 'Paris'];
   items = [];
 
+  // If no props are neede use this method
+  const message = items.length === 0 ? <p>No item found</p> : null;
+
+  //If props are needed use this method
+  const getMessage = () => {
+    return items.length === 0 ? <p>No item found</p> : null;
+  };
+
   return (
     <>
       <h1>ListGroup</h1>
-      {items.length === 0 ? <p>No item found</p> : null}
+
+      {/* if no props */}
+      {/* {message} */}
+
+      {/* If props use this */}
+      {getMessage()}
       <ul className="list-group">
         {items.map((item) => (
           <li key={item}>{item}</li>
