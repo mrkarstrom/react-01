0a012f3fcc9a9ba8dae3e3ef64a475869e7c6bf1
Magnus Karström <magnus@yesweb.se>
2024-02-20 00:38:54 +0100

    add (ListGroup.tsx): Setting class from useState.

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,17 +1,27 @@
+import { useState } from 'react';
+
 function ListGroup() {
   let items = ['New York', 'Las Vegas', 'Paris'];
   //items = [];
+
+  const [selectedIdx, setSelectedIdx] = useState(0);
 
   return (
     <>
       <h1>ListGroup</h1>
       {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
-        {items.map((item) => (
+        {items.map((item, index) => (
           <li
-            className="list-group-item"
+            className={
+              selectedIdx === index
+                ? 'list-group-item active'
+                : 'list-group-item'
+            }
             key={item}
-            onClick={() => alert('Clicked: ' + item)}
+            onClick={() => {
+              setSelectedIdx(index);
+            }}
           >
             {item}
           </li>
