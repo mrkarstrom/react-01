6cc5d2841775d9788224e5e5b8bb02d344056102
Magnus Karström <mrkarstrom81@gmail.com>
2024-02-21 12:24:02 +0100

    add (ListGroup.tsx): Added prop-checking.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
 import './App.css';
 import ListGroup from './components/ListGroup';
 
 function App() {
-  const [count, setCount] = useState(0);
+  let items = ['New York', 'Las Vegas', 'Paris'];
 
   return (
     <>
-      <ListGroup />
+      <ListGroup items={items} heading="Cities" />
     </>
   );
 }
